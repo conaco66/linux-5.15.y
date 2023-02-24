154f9ac6675f96fcc187544135322571e3f4fb1c
conaco66 <27347232@qq.com>
2023-02-24 21:43:58 +0800

    Update hid.c

--- drivers/usb/gadget/legacy/hid.c
+++ drivers/usb/gadget/legacy/hid.c
@@ -26,6 +26,102 @@
 #define HIDG_PRODUCT_NUM	0xa4ac	/* Linux-USB HID gadget */
 
 /*-------------------------------------------------------------------------*/
+//add 1
+/* hid descriptor for a keyboard */
+static struct hidg_func_descriptor my_hid_data = {
+    .subclass        = 0, /* No subclass */
+    .protocol        = 1, /* Keyboard */
+    .report_length        = 8,
+    .report_desc_length    = 63,
+    .report_desc        = {
+        0x05, 0x01,    /* USAGE_PAGE (Generic Desktop)     */
+        0x09, 0x06,    /* USAGE (Keyboard) */
+        0xa1, 0x01,    /* COLLECTION (Application) */
+        0x05, 0x07,    /* USAGE_PAGE (Keyboard) */
+        0x19, 0xe0,    /* USAGE_MINIMUM (Keyboard LeftControl) */
+        0x29, 0xe7,    /* USAGE_MAXIMUM (Keyboard Right GUI) */
+        0x15, 0x00,    /* LOGICAL_MINIMUM (0) */
+        0x25, 0x01,    /* LOGICAL_MAXIMUM (1) */
+        0x75, 0x01,    /* REPORT_SIZE (1) */
+        0x95, 0x08,    /* REPORT_COUNT (8) */
+        0x81, 0x02,    /* INPUT (Data,Var,Abs) */
+        0x95, 0x01,    /* REPORT_COUNT (1) */
+        0x75, 0x08,    /* REPORT_SIZE (8) */
+        0x81, 0x03,    /* INPUT (Cnst,Var,Abs) */
+        0x95, 0x05,    /* REPORT_COUNT (5) */
+        0x75, 0x01,    /* REPORT_SIZE (1) */
+        0x05, 0x08,    /* USAGE_PAGE (LEDs) */
+        0x19, 0x01,    /* USAGE_MINIMUM (Num Lock) */
+        0x29, 0x05,    /* USAGE_MAXIMUM (Kana) */
+        0x91, 0x02,    /* OUTPUT (Data,Var,Abs) */
+        0x95, 0x01,    /* REPORT_COUNT (1) */
+        0x75, 0x03,    /* REPORT_SIZE (3) */
+        0x91, 0x03,    /* OUTPUT (Cnst,Var,Abs) */
+        0x95, 0x06,    /* REPORT_COUNT (6) */
+        0x75, 0x08,    /* REPORT_SIZE (8) */
+        0x15, 0x00,    /* LOGICAL_MINIMUM (0) */
+        0x25, 0x65,    /* LOGICAL_MAXIMUM (101) */
+        0x05, 0x07,    /* USAGE_PAGE (Keyboard) */
+        0x19, 0x00,    /* USAGE_MINIMUM (Reserved) */
+        0x29, 0x65,    /* USAGE_MAXIMUM (Keyboard Application) */
+        0x81, 0x00,    /* INPUT (Data,Ary,Abs) */
+        0xc0        /* END_COLLECTION */
+    }
+};
+//add 1-2
+/*hid descriptor for a mouse*/
+static struct hidg_func_descriptor my_mouse_hid_data = {
+    .subclass = 0,    /*NO SubClass*/
+    .protocol = 2,    /*Mouse*/
+    .report_length = 4,
+    .report_desc_length = 52,
+    .report_desc={
+        0x05,0x01,    /*Usage Page (Generic Desktop Controls)*/
+        0x09,0x02,    /*Usage (Mouse)*/
+        0xa1,0x01,    /*Collction (Application)*/
+        0x09,0x01,    /*Usage (pointer)*/
+        0xa1,0x00,    /*Collction (Physical)*/
+        0x05,0x09,    /*Usage Page (Button)*/
+        0x19,0x01,    /*Usage Minimum(1)*/
+        0x29,0x03,    /*Usage Maximum(3) */ 
+        0x15,0x00,    /*Logical Minimum(1)*/
+        0x25,0x01,    /*Logical Maximum(1)*/
+        0x95,0x03,    /*Report Count(5)  */
+        0x75,0x01,    /*Report Size(1)*/
+        0x81,0x02,    /*Input(Data,Variable,Absolute,BitFiled)*/
+        0x95,0x01,    /*Report Count(1)*/
+        0x75,0x05,    /*Report Size(5) */
+        0x81,0x01,    /*Input(Constant,Array,Absolute,BitFiled) */
+        0x05,0x01,    /*Usage Page (Generic Desktop Controls)*/
+        0x09,0x30,    /*Usage(x)*/
+        0x09,0x31,    /*Usage(y)*/
+        0x09,0x38,    /*Usage(Wheel)*/
+        0x15,0x81,    /*Logical Minimum(-127)*/
+        0x25,0x7f,    /*Logical Maximum(127)*/
+        0x75,0x08,    /*Report Size(8)*/
+        0x95,0x02,    /*Report Count(2)  */
+        0x81,0x06,    /*Input(Data,Variable,Relative,BitFiled)*/
+        0xc0,    /*End Collection*/
+        0xc0    /*End Collection*/
+    }
+};
+
+//add 2
+static struct platform_device my_hid = {
+    .name = "hidg",
+    .id            = 0,
+    .num_resources = 0,
+    .resource    = 0,
+    .dev.platform_data = &my_hid_data,
+};
+//add 2-2
+static struct platform_device my_mouse_hid = {
+    .name = "hidg",
+    .id            = 1,
+    .num_resources = 0,
+    .resource    = 0,
+    .dev.platform_data = &my_mouse_hid_data,
+};
 
 struct hidg_func_node {
 	struct usb_function_instance *fi;
@@ -89,6 +185,7 @@ static struct usb_gadget_strings *dev_strings[] = {
 
 static int do_config(struct usb_configuration *c)
 {
+	pr_err("do_config\n");
 	struct hidg_func_node *e, *n;
 	int status = 0;
 
@@ -132,6 +229,7 @@ static struct usb_configuration config_driver = {
 
 static int hid_bind(struct usb_composite_dev *cdev)
 {
+	pr_err("hid_bind\n");
 	struct usb_gadget *gadget = cdev->gadget;
 	struct list_head *tmp;
 	struct hidg_func_node *n, *m;
@@ -206,6 +304,7 @@ put:
 
 static int hid_unbind(struct usb_composite_dev *cdev)
 {
+	pr_err("hid_unbind\n");
 	struct hidg_func_node *n;
 
 	list_for_each_entry(n, &hidg_func_list, node) {
@@ -221,6 +320,7 @@ static int hid_unbind(struct usb_composite_dev *cdev)
 
 static int hidg_plat_driver_probe(struct platform_device *pdev)
 {
+	pr_err("hidg_plat_driver_probe\n");
 	struct hidg_func_descriptor *func = dev_get_platdata(&pdev->dev);
 	struct hidg_func_node *entry;
 
@@ -241,13 +341,14 @@ static int hidg_plat_driver_probe(struct platform_device *pdev)
 
 static int hidg_plat_driver_remove(struct platform_device *pdev)
 {
+	pr_err("hidg_plat_driver_remove\n");
 	struct hidg_func_node *e, *n;
 
 	list_for_each_entry_safe(e, n, &hidg_func_list, node) {
 		list_del(&e->node);
 		kfree(e);
 	}
-
+	pr_err("hidg_plat_driver_remove return 0\n");
 	return 0;
 }
 
@@ -278,16 +379,52 @@ MODULE_LICENSE("GPL");
 
 static int __init hidg_init(void)
 {
+	pr_err("hidg_init\n");
 	int status;
 
+    //add 3
+    status = platform_device_register(&my_hid);
+    if (status < 0)
+    {
+	pr_err("platform_device_register my_hid failed\n");
+//         platform_driver_unregister(&my_hid);3
+        return status;
+    }else
+    {
+	pr_err("platform_device_register my_hid ok\n");
+    }
+    //add end 3
+    //add 3-3
+    status = platform_device_register(&my_mouse_hid);
+    if (status < 0)
+    {
+	pr_err("platform_device_register my_mouse_hid failed\n");
+//         platform_driver_unregister(&my_hid);3
+        return status;
+    }else
+    {
+	pr_err("platform_device_register my_mouse_hid ok\n");
+    }
+    //add end 3-3
 	status = platform_driver_probe(&hidg_plat_driver,
 				hidg_plat_driver_probe);
 	if (status < 0)
+	{
+		pr_err("platform_driver_probe hidg_plat_driver failed\n");
 		return status;
-
+	}else
+	{
+		pr_err("platform_driver_probe hidg_plat_driver ok\n");
+	}
 	status = usb_composite_probe(&hidg_driver);
 	if (status < 0)
+	{
+		pr_err("usb_composite_probe hidg_driver failed\n");
 		platform_driver_unregister(&hidg_plat_driver);
+	}else
+	{
+		pr_err("usb_composite_probe hidg_driver ok\n");
+	}
 
 	return status;
 }
@@ -295,7 +432,15 @@ module_init(hidg_init);
 
 static void __exit hidg_cleanup(void)
 {
+
+	pr_err("hidg_cleanup\n");
 	usb_composite_unregister(&hidg_driver);
+//add 4
+	pr_err("platform_device_unregister my_hid\n");
+	platform_device_unregister(&my_hid);
+	pr_err("platform_device_unregister my_mouse_hid\n");
+	platform_device_unregister(&my_mouse_hid);
+	pr_err("platform_driver_unregister hidg_plat_driver\n");
 	platform_driver_unregister(&hidg_plat_driver);
 }
 module_exit(hidg_cleanup);
